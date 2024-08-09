d2b00cbf15e4b97284b049dfcf0bf78ba44d0e70
Khanh Le <lednhatkhanh@gmail.com>
2024-08-09 10:45:50 +0700

    add create minwallet function

--- rust/src/minwallet.rs
+++ rust/src/minwallet.rs
@@ -1,15 +1,51 @@
+use std::collections::HashMap;
+
 use base64::prelude::*;
 use bip39::Mnemonic;
 use cardano_serialization_lib::{
-    crypto::Bip32PrivateKey,
+    address::{BaseAddress, NetworkInfo, RewardAddress, StakeCredential},
+    crypto::{Bip32PrivateKey, Bip32PublicKey},
     emip3::{decrypt_with_password, encrypt_with_password},
 };
 use rand::RngCore;
 
-use crate::network::NetworkEnvironment;
+use crate::network::{NetworkEnvironment, NetworkId};
 use crate::utils::harden;
 
-pub struct Minwallet {}
+const MINWALLET_VERSION: &str = "2.0.0";
+
+#[derive(Debug)]
+enum TransportType {
+    WEBUSB,
+    WEBHID,
+    WEBBLE,
+    BLE,
+}
+
+impl TransportType {
+    pub fn to_string(&self) -> String {
+        match self {
+            TransportType::WEBUSB => "WebUSB".to_string(),
+            TransportType::WEBHID => "WebHID".to_string(),
+            TransportType::WEBBLE => "WebBluetooth".to_string(),
+            TransportType::BLE => "Bluetooth".to_string(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Minwallet {
+    name: String,
+    base_address: HashMap<u8, String>,
+    reward_address: HashMap<u8, String>,
+    pub_key_hash_derive_map: HashMap<String, Vec<u32>>,
+    network_id: u8,
+    encrypted_key: String,
+    version: String,
+    account_index: u8,
+    wallet_name: String,
+    transport_type: Option<TransportType>,
+}
 
 impl Minwallet {
     pub fn new(
@@ -26,8 +62,136 @@ impl Minwallet {
             .derive(harden(1815))
             .derive(harden(0));
         let public_key = account_key.to_public();
-        Minwallet {}
+        let network_id = network_environment.to_network_id() as u8;
+
+        let base_address = generate_base_address(&public_key);
+        let reward_address = generate_reward_address(&public_key);
+        let pub_key_hash_derive_map = generate_addresses(&public_key);
+
+        Minwallet {
+            name: "minwallet".to_string(),
+            base_address,
+            reward_address,
+            network_id,
+            account_index: 0,
+            transport_type: None,
+            encrypted_key,
+            pub_key_hash_derive_map,
+            version: MINWALLET_VERSION.to_string(),
+            wallet_name: wallet_name.to_string(),
+        }
     }
+
+    pub fn sign(&self, raw_tx: &str) {}
+}
+
+fn generate_addresses(public_key: &Bip32PublicKey) -> HashMap<String, Vec<u32>> {
+    let mut pub_key_hash_derive_map = HashMap::new();
+    let stake_key_hash = public_key
+        .derive(2)
+        .unwrap()
+        .derive(0)
+        .unwrap()
+        .to_raw_key()
+        .hash();
+    pub_key_hash_derive_map.insert(stake_key_hash.to_hex(), vec![2, 0]);
+
+    for i in 0..1000 {
+        let receving_pub_key_hash = public_key
+            .derive(0)
+            .unwrap()
+            .derive(i)
+            .unwrap()
+            .to_raw_key()
+            .hash();
+        let change_pub_key_hash = public_key
+            .derive(1)
+            .unwrap()
+            .derive(i)
+            .unwrap()
+            .to_raw_key()
+            .hash();
+
+        pub_key_hash_derive_map.insert(receving_pub_key_hash.to_hex(), vec![0, i]);
+        pub_key_hash_derive_map.insert(change_pub_key_hash.to_hex(), vec![1, i]);
+    }
+
+    pub_key_hash_derive_map
+}
+
+fn generate_reward_address(public_key: &Bip32PublicKey) -> HashMap<u8, String> {
+    let stake_key_hash = public_key
+        .derive(2)
+        .unwrap()
+        .derive(0)
+        .unwrap()
+        .to_raw_key()
+        .hash();
+    let mut reward_address = HashMap::new();
+
+    reward_address.insert(
+        NetworkId::Mainnet as u8,
+        RewardAddress::new(
+            NetworkInfo::mainnet().network_id(),
+            &StakeCredential::from_keyhash(&stake_key_hash),
+        )
+        .to_address()
+        .to_hex(),
+    );
+
+    reward_address.insert(
+        NetworkId::Testnet as u8,
+        RewardAddress::new(
+            NetworkInfo::testnet_preprod().network_id(),
+            &StakeCredential::from_keyhash(&stake_key_hash),
+        )
+        .to_address()
+        .to_hex(),
+    );
+
+    reward_address
+}
+
+fn generate_base_address(public_key: &Bip32PublicKey) -> HashMap<u8, String> {
+    let payment_key_hash = public_key
+        .derive(0)
+        .unwrap()
+        .derive(0)
+        .unwrap()
+        .to_raw_key()
+        .hash();
+    let stake_key_hash = public_key
+        .derive(2)
+        .unwrap()
+        .derive(0)
+        .unwrap()
+        .to_raw_key()
+        .hash();
+    let mut base_address = HashMap::new();
+
+    base_address.insert(
+        NetworkId::Mainnet as u8,
+        BaseAddress::new(
+            NetworkInfo::mainnet().network_id(),
+            &StakeCredential::from_keyhash(&payment_key_hash),
+            &StakeCredential::from_keyhash(&stake_key_hash),
+        )
+        .to_address()
+        .to_hex(),
+    );
+
+    base_address.insert(
+        NetworkId::Testnet as u8,
+        BaseAddress::new(
+            NetworkInfo::testnet_preprod().network_id(),
+            &StakeCredential::from_keyhash(&payment_key_hash),
+            &StakeCredential::from_keyhash(&stake_key_hash),
+        )
+        .to_address()
+        .to_hex(),
+    );
+
+    base_address
 }
 
 fn generate_secure_string(length: usize) -> String {
@@ -54,7 +218,9 @@ fn decrypt_password(password: &str, encrypted_hex: &str) -> String {
 
 #[cfg(test)]
 mod tests {
-    use super::{decrypt_password, encrypt_password, generate_secure_string};
+    use super::{decrypt_password, encrypt_password, generate_secure_string, Minwallet};
+    use crate::bip39::generate_mnemonic;
+    use crate::network::NetworkEnvironment;
 
     #[test]
     fn can_generate_secure_string_with_length() {
@@ -63,12 +229,23 @@ mod tests {
     }
 
     #[test]
-    fn can_encrypt_password() {
+    fn can_encrypt_and_decrypt_password() {
         let password = "MyPassword@@";
         let data = "aqu23oi45ufdgsaiklojug8oasdff";
         let data_hex = hex::encode(data);
         let encrypted_hex = encrypt_password(&password, &data_hex);
         let decrypted_hex = decrypt_password(&password, &encrypted_hex);
         assert_eq!(decrypted_hex, data_hex);
+    }
+
+    #[test]
+    fn can_create_new_minwallet() {
+        let mnemonic = generate_mnemonic();
+        let password = "MyPassword@@";
+        let network_environment = NetworkEnvironment::TestnetPreprod;
+        let wallet_name = "W01";
+        let wallet = Minwallet::new(&mnemonic, password, network_environment, wallet_name);
+        println!("{:#?}", wallet);
+        assert_eq!(1, 1);
     }
 }
