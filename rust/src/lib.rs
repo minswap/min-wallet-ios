4ce6fe07c2e5b01bc162446226708d4a66a86e0d
tony <ljttl3q04t@gmail.com>
2024-12-16 15:57:56 +0700

    add more rust funcs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,16 +7,60 @@ use crate::network::NetworkEnvironment;
 use crate::wallet::embedded::WalletStaticMethods;
 use bip39::{Language, Mnemonic, MnemonicType};
 use cardano_serialization_lib::{
-    make_vkey_witness, PrivateKey, Transaction, TransactionHash, TransactionWitnessSet,
-    Vkeywitnesses,
+    make_vkey_witness, Bip32PrivateKey, PrivateKey, Transaction, TransactionHash,
+    TransactionWitnessSet, Vkeywitnesses,
 };
+use serde::{Deserialize, Serialize};
+use std::panic;
 
 // *************************** EXPORT ***************************
+#[derive(Debug, Serialize, Deserialize)]
 pub struct MinWallet {
+    wallet_name: String,
     address: String,
     network_id: u32,
     encrypted_key: String,
     account_index: u32,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct EWType {
+    wallet: EWWallet,
+    settings: EWSettings,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct EWWallet {
+    export: String,
+    version: String,
+    id: String,
+    network_id: String,
+    sign_type: String,
+    root_key: EWRootKey,
+    account_list: Vec<EWAccount>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct EWRootKey {
+    #[serde(rename = "pub")]
+    pub_key: String,
+    prv: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct EWAccount {
+    id: String,
+    #[serde(rename = "pub")]
+    pub_key: String,
+    path: (u32, u32, u32),
+    sign_type: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct EWSettings {
+    id: String,
+    network_id: String,
+    name: String,
 }
 
 pub fn gen_phrase(word_count: u32) -> String {
@@ -37,20 +81,136 @@ pub fn gen_phrase(word_count: u32) -> String {
     mnemonic.phrase().to_string()
 }
 
-pub fn create_wallet(phrase: String, password: String, network_env: String) -> MinWallet {
+pub fn create_wallet(
+    phrase: String,
+    password: String,
+    network_env: String,
+    wallet_name: String,
+) -> MinWallet {
     let network_environment = NetworkEnvironment::from_string(network_env).unwrap();
-    MinWallet::create(phrase.as_str(), password.as_str(), network_environment)
+    MinWallet::create(
+        phrase.as_str(),
+        password.as_str(),
+        network_environment,
+        wallet_name,
+    )
 }
 
 pub fn sign_tx(wallet: MinWallet, password: String, account_index: u32, tx_raw: String) -> String {
     wallet.sign_tx(password.as_str(), account_index, tx_raw)
+}
+
+pub fn change_password(
+    wallet: MinWallet,
+    current_password: String,
+    new_password: String,
+) -> MinWallet {
+    // Retrieve the root key using the current password and the wallet's encrypted key
+    let root_key =
+        MinWallet::get_root_key_from_password(current_password.as_str(), &wallet.encrypted_key);
+
+    // Generate a new encrypted key using the new password and the retrieved root key
+    let new_encrypted_key = MinWallet::gen_encrypted_key(new_password.as_str(), &root_key);
+
+    // Return a new MinWallet with the updated encrypted_key
+    MinWallet {
+        encrypted_key: new_encrypted_key,
+        ..wallet // Use the struct update syntax to copy the remaining fields
+    }
+}
+
+pub fn verify_password(wallet: MinWallet, password: String) -> bool {
+    let result = panic::catch_unwind(|| {
+        MinWallet::get_root_key_from_password(password.as_str(), &wallet.encrypted_key)
+    });
+    result.is_ok()
+}
+
+pub fn export_wallet(wallet: MinWallet, password: String, network_env: String) -> String {
+    let network_environment = NetworkEnvironment::from_string(network_env.clone()).unwrap();
+    let network_suffix = match network_environment {
+        NetworkEnvironment::Mainnet => "mm",
+        NetworkEnvironment::Preprod => "pm",
+        NetworkEnvironment::Preview => "pm",
+    };
+    let root_private_key =
+        MinWallet::get_root_key_from_password(password.as_str(), &wallet.encrypted_key);
+    let root_pub_key = root_private_key.to_public();
+    let root_account_id = root_pub_key.to_bech32().as_str()[..16].to_string();
+
+    let account_private_key = MinWallet::get_account(&root_private_key, 0);
+    let account_public_key = account_private_key.to_public();
+    let account_id = account_public_key.to_bech32().as_str()[..16].to_string();
+
+    let wallet_export = EWType {
+        wallet: EWWallet {
+            export: "minswap".to_string(),
+            version: "1.0.0".to_string(),
+            id: root_account_id.clone() + "-" + network_suffix,
+            network_id: network_env.clone(),
+            sign_type: "mnemonic".to_string(),
+            root_key: EWRootKey {
+                pub_key: root_pub_key.to_bech32(),
+                prv: root_private_key.to_hex(),
+            },
+            account_list: vec![EWAccount {
+                id: account_id,
+                pub_key: account_public_key.to_bech32(),
+                path: (1852, 1815, 0),
+                sign_type: "mnemonic".to_string(),
+            }],
+        },
+        settings: EWSettings {
+            id: root_account_id + "-" + network_suffix,
+            network_id: network_env.clone(),
+            name: wallet.wallet_name.clone(),
+        },
+    };
+    serde_json::to_string(&wallet_export).unwrap()
+}
+
+pub fn get_wallet_name_from_export_wallet(data: String) -> String {
+    let wallet_export: EWType = serde_json::from_str(data.as_str()).unwrap();
+    wallet_export.settings.name
+}
+
+pub fn import_wallet(data: String, password: String, wallet_name: String) -> MinWallet {
+    let wallet_export: EWType = serde_json::from_str(data.as_str()).unwrap();
+    let root_key_hex = wallet_export.wallet.root_key.prv;
+    let root_key = Bip32PrivateKey::from_hex(root_key_hex.as_str()).unwrap();
+    let account_index = 0;
+    let account_key = MinWallet::get_account(&root_key, account_index);
+
+    // Encrypt root key with password
+    let encrypted_key = MinWallet::gen_encrypted_key(password.as_str(), &root_key);
+
+    // Derive network ID
+    let network_env = wallet_export.wallet.network_id;
+    let network_environment = NetworkEnvironment::from_string(network_env).unwrap();
+    let network_id = network_environment.to_network_id() as u32;
+
+    // Generate addresses
+    let address = MinWallet::get_address(&account_key, network_id);
+
+    MinWallet {
+        wallet_name,
+        address: address.to_bech32(None).unwrap(),
+        network_id,
+        account_index,
+        encrypted_key,
+    }
 }
 // *************************** END EXPORT ***************************
 
 impl WalletStaticMethods for MinWallet {}
 
 impl MinWallet {
-    pub fn create(mnemonic: &str, password: &str, network_environment: NetworkEnvironment) -> Self {
+    pub fn create(
+        mnemonic: &str,
+        password: &str,
+        network_environment: NetworkEnvironment,
+        wallet_name: String,
+    ) -> Self {
         let account_index = 0;
 
         // Convert mnemonic to entropy
@@ -70,6 +230,7 @@ impl MinWallet {
         let address = MinWallet::get_address(&account_key, network_id);
 
         MinWallet {
+            wallet_name,
             address: address.to_bech32(None).unwrap(),
             network_id,
             account_index,
@@ -106,6 +267,64 @@ mod tests {
     use cardano_serialization_lib::Transaction;
 
     #[test]
+    fn test_export_wallet() {
+        let phrase =
+            "belt change crouch decorate advice emerge tongue loop cute olympic tuna donkey";
+        let password = "Minswap@123456";
+        let wallet_name = "My MinWallet".to_string();
+        let wallet = create_wallet(
+            phrase.to_string(),
+            password.to_string(),
+            "preprod".to_string(),
+            wallet_name.clone(),
+        );
+        let old_prk = wallet.get_private_key(password, 0);
+        let old_wallet_name = wallet.wallet_name.clone();
+        let old_address = wallet.address.clone();
+        let export_wallet_data = export_wallet(wallet, password.to_string(), "preprod".to_string());
+
+        let round_trip_wallet =
+            import_wallet(export_wallet_data, password.to_string(), wallet_name);
+
+        assert_eq!(old_wallet_name, round_trip_wallet.wallet_name);
+        assert_eq!(old_address, round_trip_wallet.address);
+
+        let new_prk = round_trip_wallet.get_private_key(password, 0);
+        assert_eq!(old_prk.to_hex(), new_prk.to_hex());
+    }
+    #[test]
+    fn test_verify_password() {
+        let phrase =
+            "belt change crouch decorate advice emerge tongue loop cute olympic tuna donkey";
+        let password = "123456";
+        let wallet = create_wallet(
+            phrase.to_string(),
+            password.to_string(),
+            "preprod".to_string(),
+            "My MinWallet".to_string(),
+        );
+        let is_correct = verify_password(wallet, "Wrong Password".to_string());
+        assert_eq!(is_correct, false);
+    }
+    #[test]
+    fn test_change_password() {
+        let phrase =
+            "belt change crouch decorate advice emerge tongue loop cute olympic tuna donkey";
+        let password = "123456";
+        let wallet = create_wallet(
+            phrase.to_string(),
+            password.to_string(),
+            "preprod".to_string(),
+            "My MinWallet".to_string(),
+        );
+        let private_key_1 = wallet.get_private_key(password, 0);
+
+        let new_password = String::from("Minswap@123456");
+        let new_wallet = change_password(wallet, password.to_string(), new_password.clone());
+        let private_key_2 = new_wallet.get_private_key(new_password.as_str(), 0);
+        assert_eq!(private_key_1.to_hex(), private_key_2.to_hex());
+    }
+    #[test]
     fn test_wallet_happy_case() {
         let phrase =
             "belt change crouch decorate advice emerge tongue loop cute olympic tuna donkey";
@@ -114,6 +333,7 @@ mod tests {
             phrase.to_string(),
             password.to_string(),
             "preprod".to_string(),
+            "My MinWallet".to_string(),
         );
         assert_eq!(wallet.address, "addr_test1qp5cpdqd3n6nuaa8rr8h20lnsxzx2uxdapknyh6fryl7e32e34tccc70arj2f4m9x9zdz4vu29rzzrtszalvqzpx625s2z2338".to_string());
 
