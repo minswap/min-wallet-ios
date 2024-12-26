f4486d921109ea5f58601065ed0ab98499d86d00
Cuong Nguyen <cuongnv219@gmail.com>
2024-12-26 14:00:59 +0700

    Update code

--- MinWallet/Data/Model/MinWallet+Mapper.swift
+++ MinWallet/Data/Model/MinWallet+Mapper.swift
@@ -4,8 +4,7 @@ import Foundation
 extension MinWallet: Codable {
     public init(from decoder: any Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        //TODO: cuongnv
-        //walletName = try container.decode(String.self, forKey: .walletName)
+        walletName = try container.decode(String.self, forKey: .walletName)
         address = try container.decode(String.self, forKey: .address)
         networkId = try container.decode(UInt32.self, forKey: .networkId)
         accountIndex = try container.decode(UInt32.self, forKey: .accountIndex)
@@ -14,8 +13,7 @@ extension MinWallet: Codable {
 
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-        //TODO: cuongnv
-        //try container.encode(walletName, forKey: .walletName)
+        try container.encode(walletName, forKey: .walletName)
         try container.encode(address, forKey: .address)
         try container.encode(networkId, forKey: .networkId)
         try container.encode(accountIndex, forKey: .accountIndex)
