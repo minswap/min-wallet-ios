e6d42ea26d3af7eded631e34b37a8bc23f32a612
Cuong Nguyen <cuongnv219@gmail.com>
2025-01-13 21:35:42 +0700

    Revert

--- MinWallet/Shared/Appearance/Font.swift
+++ MinWallet/Shared/Appearance/Font.swift
@@ -4,21 +4,21 @@ extension Font {
     ///20 semibold
     static let titleH6: Font = .custom("InterDisplay-SemiBold", size: 20)
     ///16 medium
-    static let labelMediumSecondary: Font = .custom("InterDisplay-Medium", size: 16)
+    static let labelMediumSecondary: Font = .custom("InterDisplay-Medium", size: 17)
     ///16 semibold
-    static let labelSemiSecondary: Font = .custom("InterDisplay-SemiBold", size: 16)
+    static let labelSemiSecondary: Font = .custom("InterDisplay-SemiBold", size: 17)
     ///14 medium
-    static let labelSmallSecondary: Font = .custom("InterDisplay-Medium", size: 14)
+    static let labelSmallSecondary: Font = .custom("InterDisplay-Medium", size: 15)
     ///14 regular
-    static let paragraphSmall: Font = .custom("InterDisplay-Regular", size: 14)
+    static let paragraphSmall: Font = .custom("InterDisplay-Regular", size: 15)
     ///14 semibold
-    static let paragraphSemi: Font = .custom("InterDisplay-SemiBold", size: 14)
+    static let paragraphSemi: Font = .custom("InterDisplay-SemiBold", size: 15)
     ///12 regular
-    static let paragraphXSmall: Font = .custom("InterDisplay-Regular", size: 12)
+    static let paragraphXSmall: Font = .custom("InterDisplay-Regular", size: 13)
     ///12 medium
-    static let paragraphXMediumSmall: Font = .custom("InterDisplay-Medium", size: 12)
+    static let paragraphXMediumSmall: Font = .custom("InterDisplay-Medium", size: 13)
     ///12 semibold
-    static let paragraphXSemiSmall: Font = .custom("InterDisplay-SemiBold", size: 12)
+    static let paragraphXSemiSmall: Font = .custom("InterDisplay-SemiBold", size: 13)
     ///24 semibold
     static let titleH5: Font = .custom("InterDisplay-SemiBold", size: 24)
     ///40 semibold
@@ -33,19 +33,19 @@ extension UIFont {
     ///20 semibold
     static let titleH6: UIFont? = UIFont(name: "InterDisplay-SemiBold", size: 20)
     ///16 medium
-    static let labelMediumSecondary: UIFont? = UIFont(name: "InterDisplay-Medium", size: 16)
+    static let labelMediumSecondary: UIFont? = UIFont(name: "InterDisplay-Medium", size: 17)
     ///16 semibold
-    static let labelSemiSecondary: UIFont? = UIFont(name: "InterDisplay-SemiBold", size: 16)
+    static let labelSemiSecondary: UIFont? = UIFont(name: "InterDisplay-SemiBold", size: 17)
     ///14 medium
-    static let labelSmallSecondary: UIFont? = UIFont(name: "InterDisplay-Medium", size: 14)
+    static let labelSmallSecondary: UIFont? = UIFont(name: "InterDisplay-Medium", size: 15)
     ///14 regular
-    static let paragraphSmall: UIFont? = UIFont(name: "InterDisplay-Regular", size: 14)
+    static let paragraphSmall: UIFont? = UIFont(name: "InterDisplay-Regular", size: 15)
     ///14 semibold
-    static let paragraphSemi: UIFont? = UIFont(name: "InterDisplay-SemiBold", size: 14)
+    static let paragraphSemi: UIFont? = UIFont(name: "InterDisplay-SemiBold", size: 15)
     ///12 regular
-    static let paragraphXSmall: UIFont? = UIFont(name: "InterDisplay-Regular", size: 12)
+    static let paragraphXSmall: UIFont? = UIFont(name: "InterDisplay-Regular", size: 13)
     ///12 medium
-    static let paragraphXMediumSmall: UIFont? = UIFont(name: "InterDisplay-Medium", size: 12)
+    static let paragraphXMediumSmall: UIFont? = UIFont(name: "InterDisplay-Medium", size: 13)
     ///24 semibold
     static let titleH5: UIFont? = UIFont(name: "InterDisplay-SemiBold", size: 24)
     ///40 semibold
