0c557b20602535bb2cd3708c53ed239d4237982c
Khanh Le <lednhatkhanh@gmail.com>
2025-01-22 10:35:56 +0700

    format code

--- MinWallet/MinWalletApp.swift
+++ MinWallet/MinWalletApp.swift
@@ -55,19 +55,20 @@ struct MinWalletApp: App {
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         // Remove this method to stop OneSignal Debugging
-       OneSignal.Debug.setLogLevel(.LL_VERBOSE)
-        
-       // OneSignal initialization
-       OneSignal.initialize("e7da5418-cbc0-4725-80d3-6400e3d09123", withLaunchOptions: launchOptions)
+        OneSignal.Debug.setLogLevel(.LL_VERBOSE)
 
-       // requestPermission will show the native iOS notification permission prompt.
-       // We recommend removing the following code and instead using an In-App Message to prompt for notification permission
-       OneSignal.Notifications.requestPermission({ accepted in
-         print("User accepted notifications: \(accepted)")
-       }, fallbackToSettings: true)
+        // OneSignal initialization
+        OneSignal.initialize("e7da5418-cbc0-4725-80d3-6400e3d09123", withLaunchOptions: launchOptions)
 
-       // Login your customer with externalId
-       // OneSignal.login("EXTERNAL_ID")
+        // requestPermission will show the native iOS notification permission prompt.
+        // We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+        OneSignal.Notifications.requestPermission(
+            { accepted in
+                print("User accepted notifications: \(accepted)")
+            }, fallbackToSettings: true)
+
+        // Login your customer with externalId
+        // OneSignal.login("EXTERNAL_ID")
         return true
     }
 
