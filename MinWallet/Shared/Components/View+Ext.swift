156fb3b777ef56025a84f8f6e372ab1e84f9cfdd
Cuong Nguyen <cuongnv219@gmail.com>
2025-02-28 11:40:13 +0700

    Fix bugs

--- MinWallet/Shared/Components/View+Ext.swift
+++ MinWallet/Shared/Components/View+Ext.swift
@@ -124,13 +124,10 @@ extension MinWalletApp {
 
 extension View {
     func openAppNotificationSettings() {
-        guard let bundleId = Bundle.main.bundleIdentifier,
-              let url = URL(string: "App-Prefs:root=NOTIFICATIONS_ID&path=\(bundleId)") else {
-            return
-        }
-        
-        if UIApplication.shared.canOpenURL(url) {
-            UIApplication.shared.open(url)
+        if let bundleIdentifier = Bundle.main.bundleIdentifier, let appSettings = URL(string: UIApplication.openSettingsURLString + bundleIdentifier) {
+            if UIApplication.shared.canOpenURL(appSettings) {
+                UIApplication.shared.open(appSettings)
+            }
         }
     }
 }
